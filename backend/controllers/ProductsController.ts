d3d32ffa8743c81a0bd3874682ee4600269e5e46
omarNaifer12 <o.naifer7@gmail.com>
2024-07-03 17:59:21 +0100

    done get product by ID  and its all images

--- backend/controllers/ProductsController.ts
+++ backend/controllers/ProductsController.ts
@@ -1,12 +1,28 @@
 import { Request, Response } from 'express';
-import {  getProductByID, getProducts, Product } from '../models/modelProducts';
+import {  getProductByID, getProducts, Product,getImagesByProductID } from '../models/modelProducts';
 export const fetchProducts = async (req: Request, res: Response): Promise<void> => {
     try {
         const products = await getProducts();
        res.status(200).json(products);
     } catch (error) {
         console.log(error);
-        
         res.status(500).json({ error: error });
     }
 };
+export const fetshOneProduct=async(req:Request,res:Response):Promise<void>=>{
+    try {
+        const productID = parseInt(req.params.id)
+        const product = await getProductByID(productID);
+
+        if (!product) {
+            res.status(404).json({ message: 'Product not found' });
+            return;
+        }
+
+        const images = await getImagesByProductID(productID);
+        res.status(200).json({ ...product, images });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error });
+    }
+}
--- backend/routes/routeProducts.ts
+++ backend/routes/routeProducts.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { fetchProducts, } from '../controllers/ProductsController';
+import { fetchProducts,fetshOneProduct } from '../controllers/ProductsController';
 
 const router = express.Router();
 
 router.get('/all', fetchProducts);
+router.get('/one/:id', fetshOneProduct);
 export default router
